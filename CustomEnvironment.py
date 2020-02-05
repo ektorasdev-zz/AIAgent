09f778321d23709a38259e463183d1eab92442de
Ektoras <hector.drandakis@gmail.com>
2020-02-05 17:19:27 +0200

    improvements

--- CustomEnvironment.py
+++ CustomEnvironment.py
@@ -4,6 +4,7 @@ import numpy as np
 import random
 
 DP_RANGE = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+SCALE_AMOUNT = 100000
 
 
 class CustomEnv(gym.Env):
@@ -18,6 +19,8 @@ class CustomEnv(gym.Env):
 
         self.df = df
         self.reward_range = (0, 10000)
+        self.total_steps = len(df) - 1
+        self.current_step = random.randint(0, len(self.df.loc[:, 'NA%'].values) - 6)
 
         # Actions of the format Increase DP x%, Decrease DP x%, Do Nothing, etc.
         self.action_space = spaces.Box(low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16)
@@ -26,67 +29,68 @@ class CustomEnv(gym.Env):
         self.observation_space = spaces.Box(low=0, high=1, shape=(720, ), dtype=np.float16)
 
     def _next_observation(self):
-        print('mpike 3')
         # Get Requests, NA, DP for the last 5 days
         frame = np.array([
-            self.df.loc[self.df['Month, Day, Year of Request Created Local'] != 'January 20, 2020', 'NA%'].values,
-            self.df.loc[self.df['Month, Day, Year of Request Created Local'] != 'January 20, 2020', 'DP'].values,
-            self.df.loc[self.df['Month, Day, Year of Request Created Local'] != 'January 20, 2020', 'Requests'].values,
+            self.df.loc[self.df['Month, Day, Year of Request Created Local'] != 'January 20, 2020', 'NA%'].values
+            / SCALE_AMOUNT,
+            self.df.loc[self.df['Month, Day, Year of Request Created Local'] != 'January 20, 2020', 'DP'].values
+            / SCALE_AMOUNT,
+            self.df.loc[self.df['Month, Day, Year of Request Created Local'] != 'January 20, 2020', 'Requests'].values
+            / SCALE_AMOUNT,
         ])
 
         # Get current Requests, NA, DP
         obs = np.append(frame, [[
-            self.df.loc[self.df['Month, Day, Year of Request Created Local'] == 'January 20, 2020', 'NA%'].values,
-            self.df.loc[self.df['Month, Day, Year of Request Created Local'] == 'January 20, 2020', 'DP'].values,
-            self.df.loc[self.df['Month, Day, Year of Request Created Local'] == 'January 20, 2020', 'Requests'].values,
+            self.df.loc[self.df['Month, Day, Year of Request Created Local'] == 'January 20, 2020', 'NA%'].values
+            / SCALE_AMOUNT,
+            self.df.loc[self.df['Month, Day, Year of Request Created Local'] == 'January 20, 2020', 'DP'].values
+            / SCALE_AMOUNT,
+            self.df.loc[self.df['Month, Day, Year of Request Created Local'] == 'January 20, 2020', 'Requests'].values
+            / SCALE_AMOUNT,
         ]])
 
         return obs
 
     def _take_action(self, action):
-        print('mpike 4')
         # Set the current price to a random price within the time step
-        self.current_dp = random.uniform(self.df.loc[self.current_step, "Requests"], self.df.loc[self.current_step, "NA%"])
+        self.current_dp = random.uniform(self.df.loc[self.current_step, "DP"], self.df.loc[self.current_step, "DP"])
 
         action_type = action[0]
         amount = action[1]
 
-        print('Action type: ', action_type)
-        print('Amount: ', amount)
-
         if action_type < 1:
-            # Buy amount % of balance in shares
-            self.new_dp = self.current_dp + 25
+            # Decrease or Hold DP Percentage
+            if self.current_dp - random.choice(DP_RANGE) <= 0:
+                self.df.loc[self.current_step, "Amount"] = 0
+            else:
+                self.df.loc[self.current_step, "Amount"] = self.current_dp - random.choice(DP_RANGE)
 
         elif action_type < 2:
-            # Sell amount % of shares held
-            if self.current_dp <= 0:
-                self.new_dp = 0
-            else:
-                self.new_dp = self.current_dp - 25
+            # Increase DP %
+            self.df.loc[self.current_step, "Amount"] = self.current_dp + random.choice(DP_RANGE)
 
     def step(self, action):
-        print('mpike 5')
         # Execute one time step within the environment
         self._take_action(action)
         self.current_step += 1
-        if self.current_step > len(self.df.loc[:, 'Requests'].values) - 6:
-            self.current_step = 0
-        delay_modifier = (self.current_step / 10000)
 
-        reward = self.balance * delay_modifier
-        done = self.net_worth <= 0
+        if 2.0 < float(self.df.loc[self.current_step, 'NA%']) < 8.0:
+            reward = 1
+        else:
+            reward = 0
+
+        done = self.total_steps <= 0
         obs = self._next_observation()
-        print('Observation: ', obs)
+        self.total_steps -= self.current_step
         return obs, reward, done, {}
 
     def reset(self):
-        print('mpike 6')
         # Reset the state of the environment to an initial state
-        self.new_dp = 0
+        self.df.loc[self.current_step, "Amount"] = 0
+        self.total_steps = self.df.shape[0]
 
         # Set the current step to a random point within the data frame
-        self.current_step = random.randint(0, len(self.df.loc[:, 'Requests'].values) - 6)
+        self.current_step = random.randint(0, len(self.df.loc[:, 'NA%'].values) - 6)
 
         return self._next_observation()
 
@@ -94,7 +98,7 @@ class CustomEnv(gym.Env):
         # Render the environment to the screen
 
         print(f'Step: {self.current_step}')
-        print(f'Balance: {self.new_dp}')
+        print(f'New DP: {self.df.loc[self.current_step, "Amount"]}')
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import datetime as dt
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
@@ -6,14 +5,14 @@ import pandas as pd
 from CustomEnvironment import CustomEnv
 
 df = pd.read_csv('Sheet12.csv')
-df = pd.DataFrame(df, columns=['City Name', 'Service type', 'Stage', 'Action', 'Day', 'From Time',
-                               'To Time', 'Amount', 'Passenger Location', 'Route Metric',
-                               'Month, Day, Year of Request Created Local', 'DP', 'Requests',
-                               'NA%', 'Non Riders'])
-df = df.sort_values('Month, Day, Year of Request Created Local')
+data = pd.DataFrame(df, columns=['City Name', 'Service type', 'Stage', 'Action', 'Day', 'From Time',
+                                 'To Time', 'Amount', 'Passenger Location', 'Route Metric',
+                                 'Month, Day, Year of Request Created Local', 'DP', 'Requests',
+                                 'NA%', 'Non Riders'])
+data = data.sort_values('Month, Day, Year of Request Created Local')
 
 # The algorithms require a vectorized environment to run
-env = DummyVecEnv([lambda: CustomEnv(df)])
+env = DummyVecEnv([lambda: CustomEnv(data)])
 model = PPO2(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=20000)
 obs = env.reset()
@@ -22,3 +21,5 @@ for i in range(2000):
     action, _states = model.predict(obs)
     obs, rewards, done, info = env.step(action)
     env.render()
+
+data.to_csv('New_file.csv', index=False)
